6086987353bc3a6e1e8877da0310c67e54b7324b
david latotzky <david-github@latotzky.de>
2018-12-19 22:21:00 +0100

    try c upd

--- tests/Scripts/setup_app.sh
+++ tests/Scripts/setup_app.sh
@@ -3,6 +3,7 @@ composer global require laravel/installer
 composer create-project --prefer-dist laravel/laravel lccTestApp
 composer config repositories.lcc vcs https://github.com/dodger451/LaravelCodeChecker
 composer require dodger451/LaravelCodeChecker:dev-master
+composer update dodger451/LaravelCodeChecker
 
 cd lccTestApp
 
