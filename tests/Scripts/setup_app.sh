6cd33b4a063a46c13f13f14875f7cb425c90bf00
david latotzky <david-github@latotzky.de>
2018-12-19 22:48:40 +0100

    add debug

--- tests/Scripts/setup_app.sh
+++ tests/Scripts/setup_app.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+echo "installing default laravel app"
 composer global require laravel/installer
 composer create-project --prefer-dist laravel/laravel lccTestApp
+
+echo "require LaravelCodeChecker from repo"
 composer config repositories.lcc vcs https://github.com/dodger451/LaravelCodeChecker
 composer require dodger451/LaravelCodeChecker:dev-master
 composer update dodger451/LaravelCodeChecker
 
-cd lccTestApp
 
+echo "publishing LaravelCodeChecker standards to config"
+cd lccTestApp
 php artisan vendor:publish --tag=laravelcodechecker
+
+echo "test cc all"
+php artisan cc:all
