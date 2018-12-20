f9c0d13cfbc492a6f7f7667805173f9f7b21485c
david latotzky <david-github@latotzky.de>
2018-12-20 22:50:45 +0100

    try  -n -d .

--- tests/Scripts/setup_app.sh
+++ tests/Scripts/setup_app.sh
@@ -2,13 +2,13 @@
 mkdir lccTestProject
 cd lccTestProject
 echo "installing default laravel app"
-composer global require laravel/installer
+composer -n -d . require laravel/installer
 composer create-project --prefer-dist laravel/laravel lccTestApp
 
 echo "require LaravelCodeChecker from repo"
 composer config repositories.lcc vcs https://github.com/dodger451/laravelcodechecker
  composer config github-oauth.github.com $1
-composer require  --prefer-dist --no-interaction dodger451/laravelcodechecker:dev-${TRAVIS_COMMIT}
+composer -n -d . require  --prefer-dist --no-interaction dodger451/laravelcodechecker:dev-${TRAVIS_COMMIT}
 
 
 echo "publishing LaravelCodeChecker standards to config"
