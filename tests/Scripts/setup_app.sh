ace5e304833aab34324df3c742fc827043f0cf3a
david latotzky <david-github@latotzky.de>
2018-12-20 22:19:11 +0100

    revert to extra checkout with TRAVIS_COMMIT

--- tests/Scripts/setup_app.sh
+++ tests/Scripts/setup_app.sh
@@ -3,6 +3,11 @@ echo "installing default laravel app"
 composer global require laravel/installer
 composer create-project --prefer-dist laravel/laravel lccTestApp
 
+echo "require LaravelCodeChecker from repo"
+composer config repositories.lcc vcs https://github.com/dodger451/laravelcodechecker
+ composer config github-oauth.github.com $1
+composer require  --prefer-dist --no-interaction dodger451/laravelcodechecker:dev-${TRAVIS_COMMIT}
+
 
 echo "publishing LaravelCodeChecker standards to config"
 cd lccTestApp
