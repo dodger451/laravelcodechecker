4d66d00877be153ea65457738ff0a4490bb85dc0
david latotzky <david-github@latotzky.de>
2018-12-20 20:48:36 +0100

    add GH_TOKEN inside setup script

--- tests/Scripts/setup_app.sh
+++ tests/Scripts/setup_app.sh
@@ -5,6 +5,7 @@ composer create-project --prefer-dist laravel/laravel lccTestApp
 
 echo "require LaravelCodeChecker from repo"
 composer config repositories.lcc vcs https://github.com/dodger451/laravelcodechecker
+composer config github-oauth.github.com $1
 composer require  --prefer-dist --no-interaction dodger451/laravelcodechecker:dev-master
 
 
